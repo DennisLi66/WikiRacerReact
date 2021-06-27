f01267d1cb8e5b1afd603bdfffb6241a67c2c06f
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-06-26 18:54:44 -0700

    Description Page and CSS added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import FormControl from 'react-bootstrap/FormControl'
+// import Form from 'react-bootstrap/Form'
+// import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 // import logo from './logo.svg';
 import './App.css';
@@ -13,7 +12,7 @@ function App() {
 const [code,changeCode] = React.useState(
   <div className='centerBox'>
     <h1> Welcome to WikiRacer </h1>
-    Button 1 <br></br>
+    Description <br></br>
     Button 2 <br></br>
     Button 3 <br></br>
   </div>
@@ -22,13 +21,39 @@ function getHome(){
   changeCode(
     <div className='centerBox'>
       <h1> Welcome to WikiRacer </h1>
-      Button 1 <br></br>
+      Description <br></br>
       Button 2 <br></br>
       Button 3 <br></br>
     </div>
   )
 }
+function getDescription(){
+  changeCode(
+    <div className="centerInfo">
+      <br></br>
+      <br></br>
+    <h1>Info on WikiRacer</h1>
 
+      Here you will find information on WikiRacing and how this site incorporates WikiRacing.
+
+    <h2>What is WikiRacing?</h2>
+
+      WikiRacing is the act of going from one Wikipedia article to another, using only hyperlinks found on the Wikipedia article, in as few hyperlinks as possible.
+
+    <h2>What is this site's version of WikiRacing?</h2>
+
+      This site uses a very simple version of WikiRacer. Once on the WikiRacer page, you can input the titles of two Wikipedia articles, get random options based on
+      our selections, or get two truly random Wikipedia Articles. If you input your own values and they are too ambiguous, you will be prompted to change them. Once you
+      have two unambiguous terms, you will be moved to the game page. You will then click links on the page until you have reached the destination page from the starting page.
+
+    <h2>What is 2Pages?</h2>
+
+      This version of WikiRacer was inspired by a website that implemented this game in same format, but the website has now gone defunct. The setup is for the most part very similar
+      to WikiRacer setup. However, the end goal for 2Pages is to make both article halves into the same article through clicking links on either side.
+
+    </div>
+  )
+}
   return (
     <div className="App">
 
@@ -41,27 +66,12 @@ function getHome(){
           style={{ maxHeight: '100px' }}
           navbarScroll
         >
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Link</Nav.Link>
-          <NavDropdown title="Link" id="navbarScrollingDropdown">
-            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="#" disabled>
-            Link
-          </Nav.Link>
+          <Nav.Link onClick={getHome}>Home</Nav.Link>
+          <Nav.Link onClick={getDescription}>Description</Nav.Link>
+          <Nav.Link href="#action2">WikiRacer</Nav.Link>
+          <Nav.Link href="#action2">2Pages</Nav.Link>
         </Nav>
-        <Form className="d-flex">
-          <FormControl
-            type="search"
-            placeholder="Search"
-            className="mr-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
+
       </Navbar.Collapse>
     </Navbar>
 
